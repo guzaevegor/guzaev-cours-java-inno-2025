96e4f64cbd3a27227514d627087d39d5681400ef
Egor-Guzaev <47603555+guzaevegor@users.noreply.github.com>
2025-10-12 11:59:20 +0300

    task2:make a getters without setters were written. a decision was made on the immutability of the fields in the class.

--- java-core/src/main/task2/Order.java
+++ java-core/src/main/task2/Order.java
@@ -4,10 +4,24 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
-  private String orderId;
-  private LocalDateTime orderDate;
-  private Customer customer;
-  private List<OrderItem> items;
-  private OrderStatus status;
+  private final String orderId;
+  private final LocalDateTime orderDate;
+  private final Customer customer;
+  private final List<OrderItem> items;
+  private final OrderStatus status;
 
+  public Order(String orderId, LocalDateTime orderDate, Customer customer,
+      List<OrderItem> items, OrderStatus status) {
+    this.orderId = orderId;
+    this.orderDate = orderDate;
+    this.customer = customer;
+    this.items = items;
+    this.status = status;
+  }
+
+  public String getOrderId() { return orderId; }
+  public LocalDateTime getOrderDate() { return orderDate; }
+  public Customer getCustomer() { return customer; }
+  public List<OrderItem> getItems() { return items; }
+  public OrderStatus getStatus() { return status; }
 }
